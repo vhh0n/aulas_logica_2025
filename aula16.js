c05964878b2a9aeb62e7d78e3c357685311b1cd2
Victor Hugo <victor.h.tavares9@aluno.senai.br>
2025-04-22 10:23:25 -0300

    Exercícios 9

---
+++ aula16.js
@@ -0,0 +1,22 @@
+//Programando a dinamica da criptografia
+let alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let segredo = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C']
+
+function criptografar(texto){
+    let resultado = ''
+    for(letra of texto){
+        let posicao = alfabeto.indexOf(letra)
+        resultado += segredo[posicao]
+    }
+    return resultado
+}
+function criptografar(texto){
+    let resultado = ''
+    for(letra of texto){
+        let posicao = segredo.indexOf(letra)
+        resultado += segredo[posicao]
+    }
+    return resultado
+}
+ console.log(criptografar('SENAI'))
+ console.log(criptografar('VHQDL'))
